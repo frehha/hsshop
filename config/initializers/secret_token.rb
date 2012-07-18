9aabb35d3fc6fae9d86b1f883a1fa1c998e7b800
frehha <christian--wenzel@web.de>
2012-07-18 16:22:27 +0200

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hsshop::Application.config.secret_token = '60d8b40e3dd88d86292dc79c51498a1b9782bb0f9c33e9d8f69ffe68192a7d28f097aad57ba8298c1cec56bec0927796fd6ed7b5c05e80ce017c204bdd767340'
